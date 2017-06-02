95ea96445227230e79617cf4245ac682a2aa93a0
Setény János <seteny.janos@tigra.hu>
2017-06-02 11:31:59 +0200

    MrStock jó sorrendben a definíciók

--- FunkyScala/src/main/scala/hu/tigra/fszk/scala/MrStocks.scala
+++ FunkyScala/src/main/scala/hu/tigra/fszk/scala/MrStocks.scala
@@ -1,27 +1,22 @@
 package hu.tigra.fszk.scala
 
-import scala.collection.GenSeq
-
 
 object MrStocks extends App {
 
-  val symbols = List("GOOG", "INTC", "AMD", "AAPL", "AMZN", "IBM", "ORCL", "MSFT")
-  findMax(symbols.par)
+  import scala.collection.GenSeq
 
-  /**
-    * https://letyourmoneygrow.com/2017/05/25/yawhore-dance-yahoo-finance/
-    * http://www.alphavantage.co/documentation/
-    */
+
   def quandlGetYearEndClosingPrice(symbol: String) = {
-    val url = s"http://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY" +
-      s"&symbol=$symbol&interval=1min&apikey=BXtYhdeyDHgj1_Kn5bz6"
+    val url =
+      s"http://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY" +
+        s"&symbol=$symbol&interval=1min&apikey=BXtYhdeyDHgj1_Kn5bz6"
 
     val data = scala.io.Source.fromURL(url).mkString.lines.toList
     val price = data(12).split("\"")(3).toDouble
     price
   }
 
-  private def findMax(symbols: GenSeq[String]) = {
+  def findMax(symbols: GenSeq[String]) = {
     val startTime = System.currentTimeMillis()
     val (topStock, topPrice) =
       symbols
@@ -32,4 +27,7 @@ object MrStocks extends App {
     println(s"${endTime - startTime} milliseconds")
   }
 
+  val symbols = List("GOOG", "INTC", "AMD", "AAPL", "AMZN", "IBM", "ORCL", "MSFT")
+
+  findMax(symbols.par)
 }
